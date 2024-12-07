2fdf738c3de1e300ab74c936fcb719ff3453d502
ajian_nedo <asianerd28@gmail.com>
2024-12-07 14:53:38 +0800

    D7P1 python one-liner lmao

--- src/d7.rs
+++ src/d7.rs
@@ -1,15 +1,5 @@
 pub fn p1(s: String) {
-    let s_ = "190: 10 19
-3267: 81 40 27
-83: 17 5
-156: 15 6
-7290: 6 8 6 15
-161011: 16 10 13
-192: 17 8 14
-21037: 9 7 18 13
-292: 11 6 16 20".to_string();
-
-    let mut lines = s.split('\n').map(|x| x.to_string()).collect::<Vec<String>>();
+    let lines = s.split('\n').map(|x| x.to_string()).collect::<Vec<String>>();
 
     let mut count = 0;
     for l in lines {
@@ -18,7 +8,7 @@ pub fn p1(s: String) {
 
         // + or *
 
-        for i in 0..2i64.pow(operands.len() as u32) {
+        for i in 0..2i64.pow(operands.len() as u32 - 1) {
             let mut total = operands[0];
             // let mut previous = operands[0];
             for t in operands.get(1..(operands.len())).unwrap().to_vec().iter().enumerate() {
@@ -45,12 +35,8 @@ pub fn p2(s: String) {
         println!("{l_index}/{}: {l}", lines.len());
         let target = l.split(":").map(|x| x.to_string()).collect::<Vec<String>>()[0].parse::<i64>().unwrap();
         let operands = l.split(":").map(|x| x.to_string()).collect::<Vec<String>>()[1].strip_prefix(" ").unwrap().split(" ").map(|x| x.to_string().parse::<i64>().unwrap()).collect::<Vec<i64>>();
-        // operands.reverse();
 
-        // + or * or ||
-
-
-        for i in 0..3i64.pow((operands.len() as u32 - 1)) {
+        for i in 0..3i64.pow(operands.len() as u32 - 1) {
             let mut mask = format_radix(i as u32, 3).chars().map(|x| x.to_string().parse::<i64>().unwrap()).collect::<Vec<i64>>();
             let mut total = operands[0];
             while mask.len() < (operands.len() - 1) {
@@ -75,14 +61,13 @@ pub fn p2(s: String) {
     println!("{count}");
 }
 
-pub fn format_radix(mut x: u32, radix: u32) -> String {
+fn format_radix(mut x: u32, radix: u32) -> String {
     let mut result = vec![];
 
     loop {
         let m = x % radix;
         x = x / radix;
 
-        // will panic if you use a bad radix (< 2 or > 36).
         result.push(std::char::from_digit(m, radix).unwrap());
         if x == 0 {
             break;
--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,5 @@ fn main() {
     // let mut x = 30;
     // println!("{}", d7::format_radix(x, 3));
 
-    d7::p2(challenge_input::input(7));
+    d7::p1(challenge_input::input(7));
 }
