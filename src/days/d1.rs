b959d5cf79ae3d22a94204bd12db5f7cdd63982d
ajian_nedo <asianerd28@gmail.com>
2025-12-01 13:50:23 +0800

    D1P1, 2

--- src/days/d1.rs
+++ src/days/d1.rs
@@ -1,3 +1,67 @@
-pub fn p1(s: String) {}
+pub fn p1(s: String) {
+    let mut count = 0;
+    let mut total = 50;
 
-pub fn p2(s: String) {}
+    for l in s.split("\n") {
+        if l.starts_with('R') {
+            let l = &l[1..].to_string().parse::<i32>().unwrap();
+            total += *l;
+        } else {
+            let l = &l[1..].to_string().parse::<i32>().unwrap();
+            total -= *l;
+        }
+
+        total %= 100;
+
+        if total == 0 {
+            count += 1;
+        }
+    }
+
+    println!("{count}");
+}
+
+pub fn p2(s: String) {
+    // p1 -> 3min
+    // p2 -> 4min
+
+    let mut count = 0;
+    let mut total = 50;
+
+    for l in s.split("\n") {
+        if l.starts_with('R') {
+            let l = &l[1..].to_string().parse::<i32>().unwrap();
+
+            for _ in 0..*l {
+                total += 1;
+
+                if total >= 100 {
+                    total = 0;
+                    count += 1;
+                }
+            }
+
+            println!("R{l} : {count} : {total}");
+        } else {
+            let l = &l[1..].to_string().parse::<i32>().unwrap();
+
+            for _ in 0..*l {
+                total -= 1;
+
+                if total < 0 {
+                    total = 99;
+                }
+
+                if total == 0 {
+                    count += 1;
+                }
+            }
+
+            println!("L{l} : {count} : {total}");
+        }
+    }
+
+    println!("{count}");
+
+    //6307 wrong
+}
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ mod challenge_input;
 mod days;
 
 fn main() {
-    days::d1::p1(challenge_input::input(1));
+    days::d1::p2(challenge_input::input(1));
 }
